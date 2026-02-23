bf74d8a54786e937b06983b3dd8b256c4469320c
Filip Sufitchi <fsufitchi@gmail.com>
2026-02-23 16:34:17 -0500

    First commit please ignore

---
+++ eslint.config.ts
@@ -0,0 +1,106 @@
+// eslint.config.js
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+import jsoncPlugin from 'eslint-plugin-jsonc'
+import vuePlugin from 'eslint-plugin-vue'
+import yamlPlugin from 'eslint-plugin-yml'
+import globals from 'globals'
+import jsoncParser from 'jsonc-eslint-parser'
+import tseslint from 'typescript-eslint'
+import vueParser from 'vue-eslint-parser'
+import yamlParser from 'yaml-eslint-parser'
+
+const compat = new FlatCompat()
+
+export default [
+  // Base
+  {
+    ignores: ['dist/', 'coverage/', '.vitepress/cache/', 'node_modules/', 'src/useHtmlMeta.ts'],
+  },
+
+  // JS defaults
+  js.configs.recommended,
+
+  // TypeScript (applies to .ts/.tsx and script blocks)
+  ...tseslint.configs.recommendedTypeChecked.map((c) => ({
+    ...c,
+    files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        sourceType: 'module',
+        tsconfigRootDir: import.meta.dirname,
+        ecmaVersion: 2023,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: ['../*', '../**/*'],
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        typescript: true,
+      },
+    },
+  })),
+
+  // Vue (SFC script + template via vue-eslint-parser)
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+
+      parserOptions: {
+        parser: tseslint.parser,
+        project: ['./tsconfig.json'],
+        extraFileExtensions: ['.vue'],
+      },
+    },
+    plugins: {
+      vue: vuePlugin,
+    },
+    rules: {
+      ...vuePlugin.configs['recommended'].rules,
+    },
+  },
+
+  // JSON / JSONC
+  {
+    files: ['**/*.json', '**/*.jsonc'],
+    languageOptions: { parser: jsoncParser },
+    plugins: { jsonc: jsoncPlugin },
+    rules: {
+      ...jsoncPlugin.configs['recommended-with-jsonc'].map((r) => r.rules),
+    },
+  },
+
+  // YAML
+  {
+    files: ['**/*.yml', '**/*.yaml'],
+    plugins: { yml: yamlPlugin },
+    languageOptions: {
+      parser: yamlParser,
+    },
+    rules: {
+      ...yamlPlugin.configs.standard.map((r) => r.rules),
+    },
+  },
+
+  // Import rules (works with TS resolver)
+  ...compat.extends('plugin:import/recommended'),
+
+  // Turn off ESLint rules that conflict with Prettier
+  ...compat.extends('prettier'),
+]
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useCounterStore } from "./stores/counter";
+
+const counterStore = useCounterStore();
+
+const increment = () => {
+  counterStore.increment();
+};
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <p>Counter: {{ counterStore.count }} <button @click="increment">Increment</button></p>
+</template>
+
+<style scoped></style>
